5f0a565e61e9e4ba7980d99287751edf02909323
W Hollowell <william@hollowell.se>
2014-03-14 16:56:04 +0100

    New page type
    Posts loop and featured area. Need to add the loop

---
+++ wp-content/themes/tyr/featured-posts.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Template name: Featured Newspage
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package Orienteringsklubben Tyr, Karlstad/Hammarö
+ */
+
+get_header( 'featured' ); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<h1>Page template not done, post loop must be inserted</h1>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
